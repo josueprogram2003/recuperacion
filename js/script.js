699aca48dba9382b60a922a71c409aeced5b495a
Samuel Cusi <scusi@gruporpp.com.pe>
2021-05-23 16:32:43 -0500

    FIX

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ let precio = document.getElementById('precio');
 let cantidad = document.getElementById('cantidad');
 let table = document.getElementById('table');
 let tbd = document.getElementById('tbd');
+let subtotal = 0;
 let filita;
 let estado = 0;
 
-function guardar() {
+function guardar(ev) {
     alert("Hola");
     if (estado == 0) {
 
@@ -16,10 +17,9 @@ function guardar() {
 
         let column_fecha = document.createElement("td");
         column_fecha.innerHTML=fecha.value;
-        console.log(columnfecha);
 
         let column_cliente = document.createElement('td');
-        column_cliente.innerHTML=cliente.value;
+        column_cliente.innerHTML=nombre.value;
 
         let column_producto = document.createElement('td');
         column_producto.innerHTML=producto.value;
@@ -32,11 +32,11 @@ function guardar() {
 
 
         let column_subtotal = document.createElement('td');
-        column_subtotal.innerHTML=column_precio*column_cantidad;
+        column_subtotal.innerHTML=precio.value * cantidad.value;
 
         let column_accion = document.createElement('td');
         column_accion.innerHTML = "<a href='#' class='icono2' onclick='editar(this)'><i class='fas fa-check'></i></a>"+"<a href='#' class='icono1' onclick='eliminar(this)'></a>"
-       
+        
         fila.appendChild(column_fecha);
         fila.appendChild(column_cliente);
         fila.appendChild(column_producto);
@@ -49,7 +49,7 @@ function guardar() {
         limpiar();
     } else {
         filita.cells[0].innerHTML = fecha.value;
-        filita.cells[1].innerHTML = cliente.value;
+        filita.cells[1].innerHTML = nombre.value;
         filita.cells[2].innerHTML = producto.value;
         filita.cells[3].innerHTML = precio.value;
         filita.cells[4].innerHTML = cantidad.value;
@@ -62,11 +62,11 @@ function guardar() {
 function editar() {
     filita = row.parentNode.parentNode;
     fecha.value = filita.cells[0].innerHTML;
-    cliente.value = filita.cells[1].innerHTML;
+    nombre.value = filita.cells[1].innerHTML;
     producto.value = filita.cells[2].innerHTML;
     precio.value = filita.cells[3].innerHTML;
     cantidad.value = filita.cells[4].innerHTML;
-    subtotal.value = filita.cells[5].innerHTML;
+    // subtotal.value = filita.cells[5].innerHTML;
     estado = 1;
 }
 
@@ -77,11 +77,11 @@ function eliminar() {
 
 function limpiar() {
     fecha.value="";
-    cliente.value="";
+    nombre.value="";
     producto.value="";
     precio.value="";
     cantidad.value="";
-    subtotal.value="";
-    accion.value="";
+    // subtotal.value="";
+    // accion.value="";
     fecha.focus();
 }
